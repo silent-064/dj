08cc9895e3dfddaf24f82a7c2f6b33363e5fd98a
Protic Prappo Durjoy <prappoprotic@gmail.com>
2025-05-26 12:05:33 +0600

    testfile

---
+++ testdri.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    if (number > 0) {
+        cout << "You entered a positive integer: " << number << endl;
+    } else if (number < 0) {
+        cout << "You entered a negative integer: " << number << endl;
+    } else {
+        cout << "You entered zero." << endl;
+    }
+
+    return 0;
+}
